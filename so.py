a2b535046f5dd20604029a5a8d0a11c6c3f3bd3b
manasasana <72486825+manasasana@users.noreply.github.com>
2021-08-26 13:21:44 +0530

    generates the following output
    Union of E and N is {0,1,2,3,4,5,6,8}
    Intersection of E and N is {2,4}
    Difference of E and N is {8,0,6}
    Symmetric difference of E and N is {0,1,3,5,6,8}

---
+++ so.py
@@ -0,0 +1,7 @@
+
+E = { 2, 4, 0, 6, 8}
+N = {1, 2, 3, 4, 5} 
+print(Union of E and N is, A | B)
+print(Intersection of E and N is, E & N)
+print(Difference of E and N is, E - N)
+print(Symmetric difference of E and N is, E ^ N)
